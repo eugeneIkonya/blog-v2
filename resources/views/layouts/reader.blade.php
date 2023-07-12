a50790e757b787c0e154af21f2d67a047e288ba8
eugeneIkonya <eugeneikonya@gmail.com>
2023-07-13 02:17:04 +0300

    google

--- resources/views/layouts/reader.blade.php
+++ resources/views/layouts/reader.blade.php
@@ -21,9 +21,8 @@
     <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/favicon/favicon-32x32.png')}}">
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/favicon/favicon-16x16.png')}}">
     <link rel="manifest" href="{{ asset('images/favicon/site.webmanifest')}}">
-
 </head>
-
+    @include('components.google.blade.php')
 <body>
     {{-- preloader --}}
     <div id="preloader">
