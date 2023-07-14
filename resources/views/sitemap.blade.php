b309a831af0f1e5ae885832a228660e421893065
eugeneIkonya <eugeneikonya@gmail.com>
2023-07-14 06:30:45 +0300

    google

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,5 +1,23 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+        <loc>{{ url('/') }}</loc>
+        <lastmod>2023-07-12T02:12:22Z</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>0.6</priority>
+    </url>
+    <url>
+        <loc>{{ url('/about') }}</loc>
+        <lastmod>2023-07-12T02:12:22Z</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>0.6</priority>
+    </url>
+    <url>
+        <loc>{{ url('/blog') }}</loc>
+        <lastmod>2023-07-12T02:12:22Z</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>0.6</priority>
+    </url>
     @foreach ($posts as $post)
         <url>
             <loc>{{ url('view/' . $post->id) }}</loc>
